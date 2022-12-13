1b91c5bc123769039786edd5f94832b1ac2cd6f1
Sparks <34066194+SparksSkywere@users.noreply.github.com>
2022-12-13 11:38:43 +0000

    Delete Write-outputverify.ps1
    Un-needed

--- Scripts/Write-outputverify.ps1
+++
@@ -1,5 +0,0 @@
-Clear-Host
-#Just a test for buttons using external code
-[System.Windows.MessageBox]::Show('The button press was successful, click Ok to close','Test','Ok','Information')
-Exit
-#Created by Chris Masters
